47be6c8c07c220e8e5ddc543e02fea45af1126f1
Ravindu Nirmal <ravindu.nirmal099@gmail.com>
2022-07-03 14:08:15 +0530

    JSX

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,13 +3,16 @@ import React from 'react';
 import './App.css'
 import Greet from './component/greet';
 import Welcome from './component/Welcome';
+import Hello from './component/hello';
 
 function App() {
   return (
-    <div className="App">
-           <Greet/>
-            <Welcome/>
-            </div> 
+          
+        <div className="App">
+           {/* <Greet/>
+            <Welcome/> */}
+            <Hello />
+         </div>  
    
   );
 }
