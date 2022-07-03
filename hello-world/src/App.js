3df1ba6d69e1dcada3179577dde9ddb931ef0ea4
Ravindu Nirmal <ravindu.nirmal099@gmail.com>
2022-07-03 13:06:04 +0530

    Functional Component(Greet function)

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import React,{ Component } from 'react';
+import Greet from './component/greet';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+       <Greet/>
+       
   );
 }
 
 export default App;
+
+// class App extends Component{
+//   render(){
+//     return(
+//       
+//       <Greet/>
+
+//     );
+//   }
+// }
+
+// export default App;
+
+
---
+++ hello-world/src/component/greet.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// function Greet(){
+
+//    return  <h1>Hello Ravindu</h1>
+
+// }
+
+// export const Greet = () => <h1>Hello Nimal</h1>
+
+const Greet = () => <h1>Hello Nimal</h1>
+
+export default Greet
